b0a6c426c41a0fc2d2ae3690def55c2c806d7e90
Lais_Schiavone <200304133441@alunos.estacio.br>
2023-08-19 13:11:46 -0300

    Concluido

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Beep(1320, 200); Console.Beep(1485, 200); Console.Beep(1650, 200); Console.Beep(1759, 200); Console.Beep(1759, 200); Console.Beep(1759, 200);
+Thread.Sleep(1000);
+Console.Beep(1320, 200); Console.Beep(1485, 200); Console.Beep(1320, 200); Console.Beep(1485, 200); Console.Beep(1485, 200); Console.Beep(1485, 200);
+Thread.Sleep(1000);
+Console.Beep(1650, 200); Console.Beep(1980, 200); Console.Beep(1759, 200); Console.Beep(1650, 200); Console.Beep(1650, 200); Console.Beep(1650, 200);
+Thread.Sleep(1000);
+Console.Beep(1320, 200); Console.Beep(1485, 200); Console.Beep(1650, 200); Console.Beep(1759, 200); Console.Beep(1759, 200); Console.Beep(1759, 200);
